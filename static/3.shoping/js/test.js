14f090c72e6ade5b38a87121c74e01b0c5be8c35
kangert <kangert@qq.com>
2020-07-20 21:14:28 +0800

    新增 学习记录

--- static/3.shoping/js/test.js
+++ static/3.shoping/js/test.js
@@ -1,3 +1,5 @@
+/* 以下代码控制台运行即可 */
+
 /**
  * 获取最大值
  */
@@ -30,3 +32,15 @@ function sort(arr) {
 }
 
 sort([1, 2, 3, 11, 21, 10, 5, 101])
+
+/**
+ * 自定义回调函数
+ */
+function arrFilter(item) {
+    return item > 5
+}
+
+[1, 3, 54, 2, 5, 7, 11].filter(arrFilter)
+
+
+
