443884d633e5b9d1357a5b21b74dc1d784e697c5
kangert <kangert@qq.com>
2020-07-20 21:07:20 +0800

    新增 学习记录

--- static/3.shoping/js/test.js
+++ static/3.shoping/js/test.js
@@ -11,4 +11,24 @@ function getMax() {
     return max
 }
 
-getMax(1,2,5,1,2,6,7,9,3,10)
+getMax(1, 2, 5, 1, 2, 6, 7, 9, 3, 10)
+
+
+/**
+ * 冒泡排序
+ */
+
+function sort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    return arr
+}
+
+sort([1, 2, 3, 11, 21, 10, 5, 101])
